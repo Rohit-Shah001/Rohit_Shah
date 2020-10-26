dfd7b83def40a6b2c0a4d65f92a3b0ec1d75dae5
Rohit Kumar Shah <rohitshah4083@gmail.com>
2020-10-26 10:26:37 +0530

    Answer to the first question

---
+++ first.py
@@ -0,0 +1,24 @@
+import sys
+# You may change this function parameters
+def findMaxProfit(numOfPredictedDay, predictedSharePrices):
+    # Participants code will be here
+    l = []
+    for i in predictedSharePrices:
+        ans = numOfPredictedDay * i
+        l.append(ans)
+    y = max(l)
+    return y
+
+def main():
+    line = input().split()
+    numOfPredictedDay = int(line[0])
+    predictedSharePrices = list(map(int, line[1:]))
+
+    answer = findMaxProfit(numOfPredictedDay, predictedSharePrices)
+
+    # Please do not remove the below line.
+    print(answer)
+    # Do not print anything after this line
+
+if __name__ == '__main__':
+    main()
